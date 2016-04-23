c80cfa1466a267cc88707359ab47f1dc1ff92e7a
Lee <lee@sodnpoo.com>
2016-04-23 15:54:35 +0100

    deal with uploads...

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,9 @@ sudo apt-get update
 sudo apt-get upgrade
 
 #install some deps
-sudo apt-get -y install git vlc-nox cmake libopencv-dev
+sudo apt-get -y install git vlc-nox cmake libopencv-dev incron
+
+cd ~
 
 #build and install ffmpeg
 git clone https://github.com/FFmpeg/FFmpeg.git
@@ -12,6 +14,7 @@ cd ffmpeg
 ./configure
 make -j5
 sudo make install
+cd ~
 
 #pull the modect userland and build the binary
 git clone https://github.com/sodnpoo/userland.git
@@ -20,6 +23,7 @@ mkdir build
 cd build
 cmake ../
 make -j5 opencv_modect
+cd ~
 
 # link in the default mask
 ln -s ~/opencv_modect_tools/mask.png ~
@@ -28,4 +32,24 @@ ln -s ~/opencv_modect_tools/mask.png ~
 mkdir ~/drop
 
 # setup modect to run at boot
-echo "@reboot pi /home/pi/opencv_modect_tools/start.sh" | sudo tee /etc/cron.d/start_opencv_modect > /dev/null
+echo "@reboot pi /home/pi/opencv_modect_tools/start.sh" | sudo tee -a /etc/cron.d/start_opencv_modect > /dev/null
+
+# sweep up any lost videos
+echo "0 */6 * * * pi /home/pi/opencv_modect_tools/sweep.sh" | sudo tee -a /etc/cron.d/start_opencv_modect > /dev/null
+
+# install youtube uploader
+sudo pip install --upgrade google-api-python-client progressbar2
+git clone https://github.com/tokland/youtube-upload.git
+cd youtube-upload/
+sudo python setup.py install
+
+echo *** dont forget to get /home/pi/client_secrets.json from google ***
+
+# incrond
+echo pi | sudo tee -a /etc/incron.allow > /dev/null
+echo "/home/pi/drop/ IN_MOVED_TO /home/pi/opencv_modect_tools/tsdotyoutube.py \$@/\$#" | sudo tee -a /var/spool/incron/pi
+sudo chown pi.incron /var/spool/incron/pi
+sudo chmod 600 /var/spool/incron/pi
+
+# set correct timezone
+sudo ln -fs /usr/share/zoneinfo/Europe/London /etc/localtime
