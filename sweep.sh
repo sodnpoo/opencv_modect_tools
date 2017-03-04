99151fe011a75adb0ab601edbe16719c61f9a4a3
Lee <lee@sodnpoo.com>
2017-03-04 08:43:55 +0000

    use s3 instead of youtube

--- sweep.sh
+++ sweep.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-for i in $( ls /home/pi/drop ); do /home/pi/opencv_modect_tools/tsdotyoutube.py /home/pi/drop/$i ; done
+#for i in $( ls /home/pi/drop ); do /home/pi/opencv_modect_tools/tsdotyoutube.py /home/pi/drop/$i ; done
+for i in $( ls /home/pi/drop ); do /home/pi/opencv_modect_tools/tsdots3.py /home/pi/drop/$i ; done
